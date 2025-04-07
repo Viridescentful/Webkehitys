593aaa5acaa4ce38dea0ea72d1118a3fddc5522c
veikk <valttejo@metropolia.fi>
2025-04-07 03:18:00 +0300

    Bello

--- Week3/Node/src/app.js
+++ Week3/Node/src/app.js
@@ -7,6 +7,19 @@ app.get('/', (req, res) => {
     res.send('Welcome to my REST API!');
 });
 
+app.get('/api/v1/cat', (req, res) => {
+  res.send({
+    "cat_id": 1,
+    "name": "Ulpukka",
+    "birthdate": "2024-01-01",
+    "weight": 4.5,
+    "owner": "Veikko",
+    "image": "https://loremflickr.com/320/240/cat",
+  });
+});
+
+app.use('/public', express.static('Week3/Node/public'));
+
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
